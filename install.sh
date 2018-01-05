c8e9fa272161ba7dfe912932281aec4eb0194025
Peter White <pwhite@delpwhite.org>
2018-01-05 00:29:06 +0000

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -x
 function setup_prereqs() {
     python3 -m venv $VIRTUALENV
     source $VIRTUALENV/bin/activate
@@ -12,13 +12,77 @@ function setup_prereqs() {
     fi
 }
 
-function setup_ap() {
-    dpkg --get-selections | grep dnsmasq || apt-get install dnsmasq
-    dpkg --get-selections | grep hostapd || apt-get install hostapd
-    systemctl stop dnsmasq
-    systemctl stop hostapd
+function_write_wlan_file() {
+cat << EOF > /etc/network/interfaces.d/wlan
+allow-hotplug wlan0
+iface wlan0 inet static
+   address 192.168.20.1
+   netmask 255.255.255.0
+   network 192.168.20.0
+
+EOF
+
+   service dhcpcd restart
+   ifdown wlan0
+   ifup wlan0
+   echo "hello"
+}
+
+function configure_dhcp() {
+   mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+cat << EOF > /etc/dnsmasq.conf
+interface=wlan0
+usually wlan0
+   dhcp-range=192.168.20.10,192.168.20.20,1h
+
+EOF
 
 }
+
+function configure_hostapd() {
+cat <<EOF >/etc/hostapd/hostapd.conf
+interface=wlan0
+driver=nl80211
+ssid=beepi
+hw_mode=g
+channel=7
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase=needtoaddvariable
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+
+EOF
+
+   sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="/etc/hostapd/hostapd.conf"/' \
+      /etc/default/hostapd
+
+   service hostapd start
+   service dnsmasq start
+   echo "hello"
+}
+
+function setup_ap() {
+    dpkg --get-selections | grep dnsmasq >/dev/null \
+	|| apt-get -y install dnsmasq
+    dpkg --get-selections | grep hostapd >/dev/null \
+        || apt-get -y install hostapd
+    systemctl stop dnsmasq
+    systemctl stop hostapd
+    grep denyinterfaces /etc/dhcpcd.conf >/dev/null \
+        || echo "denyinterfaces wlan0" >>/etc/dhcpcd.conf
+    grep -r wlan /etc/network >/dev/null \
+        || function_write_wlan_file
+    [[ -f /etc/dnsmasq.conf.orig ]] \
+        || configure_dhcp
+    [[ -f /etc/hostapd/hostapd.conf ]] \
+        || configure_hostapd
+}
+
 function setup_service() {
     touch $INIT_FILE
     chmod 0700 $INIT_FILE
