5934be3b00961c7dcf4866cf7b98cfaebe6c1e10
erikdeirdre <pwhite@delpwhite.org>
2018-12-14 17:23:43 -0500

    Cleanup Pep8 problems

---
+++ tests/test_config.py
@@ -0,0 +1,13 @@
+from config import get_probe_types
+
+
+def test_load_config():
+    assert True
+
+
+def test_write_config():
+    assert True
+
+
+def test_get_probe_types():
+    assert get_probe_types() == [0, 11, 22, 2302]
