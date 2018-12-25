eda96aac01035b1de5bee8d027e0947fedd3f1e9
erikdeirdre <pwhite@delpwhite.org>
2018-12-25 16:02:00 -0500

    update requirements

--- install.sh
+++ install.sh
@@ -9,20 +9,24 @@ function setup_python3() {
 }
 
 function copy_files() {
+    if [[ ! -d "${BEE_SRC}" ]]; then
+        mkdir -p ${BEE_SRC}
+    fi
+
     for FILE in cmd_config.py config.py record_data.py gui_config.py install.sh setup.py
     do
-        cp ${FILE} ${BEE_DIR}/src/.
+        cp ${FILE} ${BEE_SRC}/.
     done
 }
 
 function setup_prereqs() {
     echo "Creating virtualenv ${VIRTUALENV}"
-    if [[ ! -d ${VIRTUALENV} ]]; then
+    if [[ ! -d "${VIRTUALENV}" ]]; then
       python3 -m venv ${VIRTUALENV}
     fi
     echo "Installing python requirements"
     source ${VIRTUALENV}/bin/activate
-    pip install -r requirements.txt
+    pip install -r pi_requirements.txt
     echo "Searching for probes"
     python find_probes.py
     if [[ $? -eq 0 ]]; then
@@ -49,8 +53,8 @@ StandardError=syslog
 SyslogIdentifier=bee_data
 Environment=VIRTUAL_ENV=${VIRTUALENV}
 Environment=PATH=${VIRTUAL_ENV}/bin:${PATH}
-Environment=DATA_DIR=${BEE_DIR}/bee_data
-ExecStart=${VIRTUALENV}/bin/python ${BEE_DIR}/src/record_data.py
+Environment=DATA_DIR=${BEE_DATA}
+ExecStart=${VIRTUALENV}/bin/python ${BEE_SRC}/record_data.py
 Restart=always
 
 [Install]
@@ -96,9 +100,11 @@ RC=0
 INIT_FILE=/usr/lib/systemd/system/bee_data.service
 CONF_FILE=/etc/rsyslog.d/bee_data.conf
 BEE_DIR=/opt/bee_pi
+BEE_DATA=${BEE_DIR}/bee_data
+BEE_SRC=${BEE_DIR}/src
 VIRTUALENV=${BEE_DIR}/virtualenv
 
-[[ ! -d ${BEE_DIR}/bee_data ]] && mkdir ${BEE_DIR}/bee_data
+[[ ! -d ${BEE_DATA} ]] && mkdir -p ${BEE_DATA}
 
 copy_files
 setup_prereqs
